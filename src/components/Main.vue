cc179b0bd33116f9c9a41cc8a5eecdbffa0eec14
Dmytro1231 <dimarubalchenk@ukr.net>
2021-02-18 11:39:23 +0200

    commit

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,16 +2,16 @@
   <main>
     <div class="container">
       <div class="length">
-        <h1>Количество постов: {{ users.length }}</h1>
+        <h1>Количество постов: {{ posts.length }}</h1>
       </div>
-      <div class="card center" v-for="user in users" :key="user.id">
-        <h1>{{ user.title }}</h1>
-        <p>{{ user.body }}</p>
-        <span>{{ user.userId }}</span>
+      <div class="card center" v-for="post in posts" :key="post.id">
+        <h1>{{ post.title }}</h1>
+        <p>{{ post.body }}</p>
+        <span>{{ post.postId }}</span>
         <router-link
           :to="{
             name: 'post',
-            params: { id: user.id, date: user.title, date2: user.body },
+            params: { id: post.id, date: post.title, date2: post.body },
           }"
         >
           <button class="btn primary">Читать пост</button>
@@ -28,13 +28,13 @@ export default {
   name: 'Main',
   data () {
     return {
-      users: []
+      posts: []
     }
   },
   mounted () {
     axios
       .get('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => (this.users = response.data))
+      .then((response) => (this.posts = response.data))
       .catch((error) => {
         console.log(error)
       })
